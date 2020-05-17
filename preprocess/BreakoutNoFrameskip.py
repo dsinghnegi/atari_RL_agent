89afb507afda52afefeed6f4662335f2abc40359
dsinghnegi <negidhirendersingh26@gmail.com>
2020-05-17 23:22:56 +0530

    preprocessing file for breakout

---
+++ preprocess/BreakoutNoFrameskip.py
@@ -0,0 +1,66 @@
+from gym.core import ObservationWrapper
+from gym.spaces import Box
+import cv2
+
+import atari_wrappers
+
+class PreprocessAtariObs(ObservationWrapper):
+    def __init__(self, env):
+        """A gym wrapper that crops, scales image into the desired shapes and grayscales it."""
+        ObservationWrapper.__init__(self, env)
+
+        self.img_size = (1, 64, 64)
+        self.observation_space = Box(0.0, 1.0, self.img_size)
+
+
+    def _to_gray_scale(self, rgb, channel_weights=[0.8, 0.1, 0.1]):
+        gray=rgb[:,:,0]*channel_weights[0]+rgb[:,:,1]*channel_weights[1]+rgb[:,:,2]*channel_weights[2]
+        return gray 
+
+
+    def observation(self, img):
+        """what happens to each observation"""
+
+        # Here's what you need to do:
+        #  * crop image, remove irrelevant parts
+        #  * resize image to self.img_size
+        #     (use imresize from any library you want,
+        #      e.g. opencv, skimage, PIL, keras)
+        #  * cast image to grayscale
+        #  * convert image pixels to (0,1) range, float32 type
+        img=img[30:-15,:]
+        img=cv2.resize(img,(64,64))
+        
+        img=img.astype('float32')/255.0
+        img=self._to_gray_scale(img)
+        # print(img.shape)
+
+        return img.reshape(-1,64,64)
+
+
+def PrimaryAtariWrap(env, clip_rewards=True):
+    assert 'NoFrameskip' in env.spec.id
+
+    # This wrapper holds the same action for <skip> frames and outputs
+    # the maximal pixel value of 2 last frames (to handle blinking
+    # in some envs)
+    env = atari_wrappers.MaxAndSkipEnv(env, skip=4)
+
+    # This wrapper sends done=True when each life is lost
+    # (not all the 5 lives that are givern by the game rules).
+    # It should make easier for the agent to understand that losing is bad.
+    env = atari_wrappers.EpisodicLifeEnv(env)
+
+    # This wrapper laucnhes the ball when an episode starts.
+    # Without it the agent has to learn this action, too.
+    # Actually it can but learning would take longer.
+    env = atari_wrappers.FireResetEnv(env)
+
+    # This wrapper transforms rewards to {-1, 0, 1} according to their sign
+    if clip_rewards:
+        env = atari_wrappers.ClipRewardEnv(env)
+
+    # This wrapper is yours :)
+    env = PreprocessAtariObs(env)
+    return env
+
