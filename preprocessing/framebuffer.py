cfd862d400fd579e8d1664bdecf5c661e5db3ce2
dsinghnegi <negidhirendersingh26@gmail.com>
2020-07-26 17:43:06 +0530

    fixing frames

--- preprocessing/framebuffer.py
+++ preprocessing/framebuffer.py
@@ -4,25 +4,17 @@ from gym.core import Wrapper
 
 
 class FrameBuffer(Wrapper):
-    def __init__(self, env, n_frames=4, dim_order='tensorflow'):
+    def __init__(self, env, n_frames=4):
         """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
         super(FrameBuffer, self).__init__(env)
-        self.dim_order = dim_order
-        if dim_order == 'tensorflow':
-            height, width, n_channels = env.observation_space.shape
-            obs_shape = [height, width, n_channels * n_frames]
-        elif dim_order == 'pytorch':
-            n_channels, height, width = env.observation_space.shape
-            obs_shape = [n_channels * n_frames, height, width]
-        else:
-            raise ValueError(
-                'dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
+        n_channels, height, width = env.observation_space.shape
+        obs_shape = [n_channels * n_frames, height, width]
         self.observation_space = Box(0.0, 1.0, obs_shape)
         self.framebuffer = np.zeros(obs_shape, 'uint8')
 
     def reset(self):
         """resets breakout, returns initial frames"""
-        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.framebuffer = None
         self.update_buffer(self.env.reset())
         return self.framebuffer
 
@@ -30,16 +22,11 @@ class FrameBuffer(Wrapper):
         """plays breakout for 1 step, returns frame buffer"""
         new_img, reward, done, info = self.env.step(action)
         self.update_buffer(new_img)
+
         return self.framebuffer, reward, done, info
 
     def update_buffer(self, img):
-        if self.dim_order == 'tensorflow':
-            offset = self.env.observation_space.shape[-1]
-            axis = -1
-            cropped_framebuffer = self.framebuffer[:, :, :-offset]
-        elif self.dim_order == 'pytorch':
-            offset = self.env.observation_space.shape[0]
-            axis = 0
-            cropped_framebuffer = self.framebuffer[:-offset]
-        self.framebuffer = np.concatenate(
-            [img, cropped_framebuffer], axis=axis)
+        if self.framebuffer is None:
+            self.framebuffer=np.repeat(img, 4, axis=0)
+        self.framebuffer = np.append(self.framebuffer[1:, :, :], img, axis=0)
+      
