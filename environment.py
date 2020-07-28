663d38d4de942f14e89687269aa55771dd2e035c
dsinghnegi <negidhirendersingh26@gmail.com>
2020-07-28 22:14:22 +0530

    KungFuMasterDeterministic environment

--- environment.py
+++ environment.py
@@ -1,7 +1,9 @@
 from preprocessing import PongDeterministic as PD
 from preprocessing import BreakoutDeterministic as BD
+from preprocessing import KungFuMasterDeterministic as KD
 
 ENV_DICT={
 'PongDeterministic-v4':PD,
 'BreakoutDeterministic-v4':BD,
+'KungFuMasterDeterministic-v4':KD,
 }
