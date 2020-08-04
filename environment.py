5ca50c37626b98b187aefedbead3ec88a42b7897
dsinghnegi <negidhirendersingh26@gmail.com>
2020-08-04 08:22:31 +0530

    Adding support file for SpaceInvaderDeterministic

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 from preprocessing import PongDeterministic as PD
 from preprocessing import BreakoutDeterministic as BD
 from preprocessing import KungFuMasterDeterministic as KD
-from preprocessing import BoxingDeterministic
+from preprocessing import BoxingDeterministic, SpaceInvadersDeterministic
 
 
 ENV_DICT={
@@ -9,4 +9,5 @@ ENV_DICT={
 'BreakoutDeterministic-v4':BD,
 'KungFuMasterDeterministic-v4':KD,
 'BoxingDeterministic-v4':BoxingDeterministic,
+'SpaceInvadersDeterministic-v4':SpaceInvadersDeterministic,
 }
