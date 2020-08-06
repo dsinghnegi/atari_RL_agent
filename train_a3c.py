61808369ce2a046f30157b105dacf936d2d4da97
dsinghnegi <negidhirendersingh26@gmail.com>
2020-08-06 20:00:08 +0530

    lstm support for training

--- train_a3c.py
+++ train_a3c.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 from models import A3C, EnvBatch
 from utils import utils
-from utils.helper import evaluate_A3C
+from utils.helper import evaluate_A3C,evaluate_A3C_lstm
 from utils.loss import compute_A3C_loss
 import environment 
 import tpu
@@ -26,12 +26,14 @@ def get_args():
 	ap.add_argument("-t", "--train_dir", default="train_dir", help="Checkpoint directory")
 	ap.add_argument("-c", "--checkpoint", default=None, help="Checkpoint for agent")
 	ap.add_argument("--tpu", action='store_true', help="Enable TPU")
-	ap.add_argument("--lr", default=1e-4, type=float, help="Learning Rate")
+	ap.add_argument("--lr", default=1e-5, type=float, help="Learning Rate")
 	ap.add_argument("--max_grad_norm", default=20.0,type=float, help="Gradient clipping")
 	ap.add_argument("--gamma", default=0.99, type=float, help="Discounting factor")	
 	ap.add_argument("--steps", default=int(10e5), type=int, help="Training steps")
 	ap.add_argument("--loss_freq", default=50, type=int, help="loss frequency")
 	ap.add_argument("--eval_freq", default=2500, type=int, help="Evalualtion frequency")
+	ap.add_argument("--lstm", action='store_true', help="Enable LSTM")
+
 	
 	opt = ap.parse_args()
 	return opt
@@ -54,33 +56,40 @@ def train(make_env, EnvBatch, agent, device, writer, opt):
 
 	optim = torch.optim.Adam(agent.parameters(), lr=opt.lr)
 
-	env = make_env(clip_rewards=True)
+	env = make_env(lstm=opt.lstm, clip_rewards=True)
 	step = 0
 	state = env.reset()
 	if opt.checkpoint:
 		agent.load_state_dict(torch.load(opt.checkpoint))
 		step=int(re.findall(r'\d+', opt.checkpoint)[-1])
 
-	
+	evaluate= evaluate_A3C_lstm if opt.lstm else evaluate_A3C	
 
-	score=evaluate_A3C(make_env(clip_rewards=False), agent)
+	score=evaluate(make_env(lstm=opt.lstm, clip_rewards=False), agent)
 	print("Score without training: {}".format(score))
 
 
-	env_batch = EnvBatch(make_env=make_env,n_envs=64)
+	env_batch = EnvBatch(make_env=make_env, n_envs=128, clip_rewards=True, lstm=opt.lstm)
 	batch_states = env_batch.reset()
 	grad_norm=0
+	hidden_unit=None
 	for step in trange(step,total_steps + 1):
 		optim.zero_grad()
+		with torch.no_grad():
+			if opt.lstm:
+				agent_outputs, hidden_unit = agent(batch_states, hidden_unit)
+			else:
+				agent_outputs = agent(batch_states)
 
-		agent_outputs = agent(batch_states)
 		batch_actions = agent.sample_actions(agent_outputs)
 		batch_next_states, batch_rewards, batch_done, _ = env_batch.step(batch_actions)
-		batch_rewards=batch_rewards.astype(np.float32)*0.01
+		# print(batch_rewards)
+		# batch_rewards=batch_rewards.astype(np.float32)*0.01
 	
 		loss, entropy = compute_A3C_loss(batch_states, batch_actions, batch_rewards, 
 											batch_next_states, batch_done, agent,
-											gamma=gamma,	device=device)
+											hidden_unit=hidden_unit,
+											gamma=gamma,device=device)
 		if loss >0:
 			loss.backward()
 			grad_norm = nn.utils.clip_grad_norm_(agent.parameters(), max_grad_norm)
@@ -99,7 +108,7 @@ def train(make_env, EnvBatch, agent, device, writer, opt):
 			
 
 		if step % eval_freq == 0:
-			mean_rw=evaluate_A3C(make_env(clip_rewards=False), agent)
+			mean_rw=evaluate(make_env(clip_rewards=False, lstm=opt.lstm), agent)
 			print("MEAN REWARD:{}".format(mean_rw))
 			writer.add_scalar("Mean reward", mean_rw, step)
 			writer.close()
@@ -126,12 +135,12 @@ def main():
 
 	ENV=environment.ENV_DICT[opt.environment]
 
-	env = ENV.make_env()
+	env = ENV.make_env(lstm=opt.lstm)
 	state_shape = env.observation_space.shape
 	n_actions = env.action_space.n
 		
 
-	agent = A3C(n_actions=n_actions).to(device)
+	agent = A3C(n_actions=n_actions, lstm=opt.lstm).to(device)
 	
 	writer.add_graph(agent, torch.tensor([env.reset()]).to(device))
 	writer.close()
